31766ac8aad4abc91e005fcaafbb48525a39d716
Kevin Quiroz <kquirozg@eafit.edu.co>
2023-08-02 11:36:16 -0500

    Nuevo HTML

--- movie/views.py
+++ movie/views.py
@@ -8,3 +8,6 @@ def home(request):
     #return render (request, 'home.html')
     return render (request, 'home.html', {'name': 'Kevin Quiroz'})
 
+def about(request):
+      #return HttpResponse('<h1>Movie Review About</h1>')
+      return render (request, 'about.html')
